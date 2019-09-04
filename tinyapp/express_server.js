517cd9b657743090d3ac1319b5ffe51251edf152
Andrew <andrew.j.hui@gmail.com>
2019-09-04 22:51:48 +0000

    committing branch work preparing for merge

--- tinyapp/express_server.js
+++ tinyapp/express_server.js
@@ -13,7 +13,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString() {
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
+function generateRandomString() { 
   let result = [];
   let arrayString = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
   for (let i = 0; i < 6; i++) {
@@ -24,13 +37,23 @@ function generateRandomString() {
   return result.join('')
 }
 
+function emailLookup(emailCheck) {
+  for (let par in users) {
+    if (emailCheck === users[par].email) {
+      return users[par];
+    }
+  }
+  return false;
+}
+
+
 // app.get("*", (req, res) => {
 //   let templateVars = {urls: urlDatabase}
 //   res.redirect("/urls")
 // });
 
 
-
+// GETS
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
@@ -59,6 +82,23 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars );
 });
 
+app.get("/register", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  }
+
+  res.render("urls_register", templateVars);
+})
+
+app.get("/login", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  }
+  res.render("urls_login", templateVars);
+})
+
 
 app.post("/urls", (req, res) => { // creating 6 alphanumeric and adds to db
   let genURL = generateRandomString();
@@ -78,18 +118,39 @@ app.post("/urls/:shortURL", (req, res) => { // update the long URL
   res.redirect("/urls");
 })
 
-app.post("/login", (req, res) => { // LOGIN
-  let username = req.body.username;
-  res.cookie("username", username);
-  res.redirect("/urls")
-})
-
 app.post("/clearCookie", (req, res) => { // clear cookies
   res.clearCookie("username");
   res.redirect("/urls")
 })
 
+app.post("/register", (req, res) => { // register users to the database
+let genID = generateRandomString();
+let em = req.body.email;
+if (em === "" || req.body.password === "" || emailLookup(em).email === em) {
+  res.status(400).send("STATUS: 400. Please go back and enter a unique email and password!!");
+}
+else {
+  users[genID] = {
+    id: genID,
+    email: req.body.email,
+    password: req.body.password
+  }
+  let username = users[genID].email;
+  res.cookie("username", username);
+  res.redirect("/urls")
+}
+})
 
+app.post("/login", (req, res) => { //login into database
+  let objectID = emailLookup(req.body.email);
+  if(objectID.email === req.body.email && objectID.password === req.body.password) {
+    res.cookie("username", objectID.email)
+    res.redirect("/urls")
+  }
+  else {
+    res.status(403).send("STATUS: 403. Please re-login");
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
